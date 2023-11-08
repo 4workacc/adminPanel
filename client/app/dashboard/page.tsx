86006766238392dfe612a6fb3780c801b805e937
Eugen Svirbutovich <4workacc@gmail.com>
2023-11-08 17:08:15 +0300

    start nav

---
+++ client/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+import styles from './dashboard.module.css'
+
+export default function Dashboard() {
+  return (    
+      <div className={styles.dashboard}>
+        <div className={styles.menu}>menu</div>
+        <div className={styles.main}>main</div>
+      </div>   
+  )
+}
