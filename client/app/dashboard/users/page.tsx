28a019e0dde17b2f49e144a06f5feb000048b29a
Eugen Svirbutovich <4workacc@gmail.com>
2023-11-09 15:51:57 +0300

    feat: simple dashboard styles
    28:27

---
+++ client/app/dashboard/users/page.tsx
@@ -0,0 +1,18 @@
+export default function Users() {
+    return (
+        <div>
+            Users
+        </div>
+    )
+}
+
+/**
+ * 
+ * const UsersPage = () => {
+ *  return (
+ *      <div>UsersPage</div>
+ *  )
+ * }
+ * 
+ * export default UsersPage
+ */
---
+++ client/app/ui/dashboard/sidebar/sidebar.tsx
@@ -0,0 +1,83 @@
+import Image from 'next/image.js';
+import MenuLink from './menuLink/menuLink.jsx';
+import styles from './sidebar.module.css';
+import {
+  MdSupervisedUserCircle,
+  MdDashboard,
+  MdShoppingBasket,
+  MdAttachMoney,
+  MdInsertChartOutlined,
+  MdSecurity
+} from "react-icons/md";
+
+const menuItems = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: "Tests",
+        path: "/dashboard/Tests",
+        icon: <MdShoppingBasket />,
+      },
+      {
+        title: "Transactions",
+        path: "/dashboard/transations",
+        icon: <MdAttachMoney />
+      }
+    ]
+  },
+  {
+    title: "Analitics",
+    list: [
+      {
+        title: "Revenue",
+        path: "/dashboard/revenue",
+        icon: <MdInsertChartOutlined />
+      }
+    ]
+  },
+  {
+    title: "User",
+    list: [
+      {
+        title: "Settings",
+        path: "/dashboard/settings",
+        icon: <MdSecurity />
+      }
+    ]
+  }
+]
+
+export default function Sidebar() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image className = { styles.userImage} src="/userIcon.png" alt="" width="50" height="50" /> 
+        <div className={styles.userDetail}>
+          <span className={styles.userName}>XXX</span>
+          <span className={styles.userTitle}>ADM</span>
+        </div>        
+      </div>
+      <ul className={styles.list}>
+      {menuItems.map(cat=>(       
+          <li key={cat.title}>
+          <span className={styles.cat}>{cat.title}</span> 
+          {cat.list.map(item=>(
+            <MenuLink item={item} />
+          ))}
+          </li>        
+      ))}
+      </ul>
+    </div>
+  )
+}
