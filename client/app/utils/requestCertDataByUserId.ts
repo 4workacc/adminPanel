b20be3ca305e7b26740a3bf76c21224a1cfc298b
Eugen Svirbutovich <4workacc@gmail.com>
2023-11-28 15:32:04 +0300

    add

---
+++ client/app/utils/requestCertDataByUserId.ts
@@ -0,0 +1,34 @@
+import { Client, PoolClient } from "pg"
+import conn from "./db"
+import { IUser } from "./types";
+
+export const requestUserDataById = async (requestUserId: number) => {
+    const client: PoolClient = await conn.connect();
+    try {
+        const result = await client.query({
+            rowMode: 'array',
+            text: `SELECT * FROM users where user_id = ${requestUserId}`,
+        })
+        const newUser: IUser = {
+            id: result.rows[0][0],
+            fio: result.rows[0][1],
+            code0: result.rows[0][2] || -1,
+            code1: result.rows[0][3] || "",
+            employement: result.rows[0][4] || "",
+            struct: result.rows[0][5] || "",
+            post: result.rows[0][6] || "",
+            unit: result.rows[0][7] || "",
+            domain_name: result.rows[0][8] || "",
+            status: result.rows[0][9] || "",
+            old_fam: result.rows[0][10] || "",
+            reason_of_code: result.rows[0][11] || ""
+        };
+     return newUser
+    }
+    catch (Err) { }
+    finally {
+        await client.release()
+    }
+}
+
+
--- client/app/utils/types.ts
+++ client/app/utils/types.ts
@@ -12,3 +12,17 @@ export interface IUser{
     old_fam?: string;
     reason_of_code?: string;
 }
+export interface IKey {
+    id: number;
+    key_id: string;
+    user_id: number;
+    key_sn: string;
+    start_cert: string;
+    end_cert: string;
+    active: boolean;
+    status: string;
+    info: string;
+    cert_id: string;
+    doc_employement: string;
+    doc_fam: string;
+}
